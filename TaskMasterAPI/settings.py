549f38d958b9519c6392619e41f58a6e8110700f
benjamin <dumstick228@gmail.com>
2018-08-20 09:15:57 +0200

    Heroku files

--- TaskMasterAPI/settings.py
+++ TaskMasterAPI/settings.py
@@ -28,7 +28,7 @@ with open('secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["evening-shelf-60759.herokuapp.com"]
 
 
 # Application definition
